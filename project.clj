b90f6138427fcea5668f69ceb8e8f91943ba8ecb
David James <david@bluemontlabs.com>
2013-12-19 20:15:45 -0500

    point to correct Github repo, add license

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject echo-byte "0.1.0-SNAPSHOT"
   :description "Basic TCP echo client/server in Clojure using Java 7 NIO.2"
-  :url "https://github.com/xpe/clj-tcp-echo"
+  :url "https://github.com/bluemont/clj-async-tcp-echo-nio.2"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev

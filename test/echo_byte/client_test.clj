ebfb8fd489322dc4b5e4a35c828eba9c10b2c144
David James <david@bluemontlabs.com>
2013-12-19 20:15:01 -0500

    remove old tests

--- test/echo_byte/client_test.clj
+++
@@ -1,7 +0,0 @@
-(ns echo-byte.client-test
-  (:require [clojure.test :refer :all]
-            [echo-byte.client :as client]))
-
-; (deftest a-test
-;   (testing "FIXME, I fail."
-;     (is (= 0 1))))

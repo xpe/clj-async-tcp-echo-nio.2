846f7567dc32f2517538852569e22ee11972d35d
David James <david@bluemontlabs.com>
2015-05-11 18:03:11 -0400

    use Clojure 1.7.0-beta2

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/bluemont/clj-async-tcp-echo-nio.2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev
-             {:source-paths ["dev"]
-              :dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]]
+  :profiles
+  {:dev
+   {:source-paths ["dev"]
+    :dependencies [[org.clojure/tools.namespace "0.2.10"]]}})
